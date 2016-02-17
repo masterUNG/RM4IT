a5d3e4f10fecb853431a9417e7c2ff8b9dc5d5bd
masterUNG <phrombutr@gmail.com>
2016-02-17 17:47:38 +0700

    Design UI จวนเสร็จ

---
+++ app/src/main/java/appewtc/masterung/rm4it/MyManage.java
@@ -0,0 +1,24 @@
+package appewtc.masterung.rm4it;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by masterUNG on 2/17/16 AD.
+ */
+public class MyManage {
+
+    //Explicit
+    private MyOpenHelper myOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public MyManage(Context context) {
+
+        //Connected Database
+        myOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = myOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = myOpenHelper.getReadableDatabase();
+
+    }   // Constructor
+
+}   // Main Class
