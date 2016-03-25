074a58bd617c1d6fe6a3b996bd8d0ffc4c28df38
masterUNG <phrombutr@gmail.com>
2016-03-25 11:13:59 +0700

    Syn JSON to SQLite finish

--- app/src/main/java/appewtc/masterung/rm4it/MainActivity.java
+++ app/src/main/java/appewtc/masterung/rm4it/MainActivity.java
@@ -2,9 +2,23 @@ package appewtc.masterung.rm4it;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +39,81 @@ public class MainActivity extends AppCompatActivity {
         //Delete All SQLite
         deleteAllSQLite();
 
+        //Syn JSON to SQLite
+        synJSONtoSQLite();
+
+
     }   // Main Method
+
+    private void synJSONtoSQLite() {
+
+        //Connected Http
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        //1 Create Input Stream
+        InputStream inputStream = null;
+        String urlJSON = "http://swiftcodingthai.com/rm4it/php_get_user_master.php";
+        String tag = "Rm4it";
+        try {
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(urlJSON);
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            inputStream = httpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d(tag, "Input ==> " + e.toString());
+        }
+
+        //2 Create JSON String
+        String strJSON = null;
+        try {
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            StringBuilder stringBuilder = new StringBuilder();
+            String strLine = null;
+
+            while ((strLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(strLine);
+            }   // while
+            inputStream.close();
+            strJSON = stringBuilder.toString();
+
+        } catch (Exception e) {
+            Log.d(tag, "strJSON ==> " + e.toString());
+        }
+
+
+        //3 Update SQLite
+        try {
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String strUser = jsonObject.getString(MyManage.column_User);
+                String strPassword = jsonObject.getString(MyManage.column_Password);
+                String strName = jsonObject.getString(MyManage.column_Name);
+                String strIDcard = jsonObject.getString(MyManage.column_ID_card);
+                String strProvince = jsonObject.getString(MyManage.column_Province);
+                String strPosition = jsonObject.getString(MyManage.column_Position);
+                String strWorkYear = jsonObject.getString(MyManage.column_Work_Year);
+                String strEmail = jsonObject.getString(MyManage.column_Email);
+
+                myManage.addUser(strUser, strPassword, strName, strIDcard, strProvince,
+                        strPosition, strWorkYear, strEmail);
+
+            }   //for
+
+        } catch (Exception e) {
+            Log.d(tag, "Update ==> " + e.toString());
+        }
+
+
+    }   // synJSONtoSQLite
 
     private void deleteAllSQLite() {
 
