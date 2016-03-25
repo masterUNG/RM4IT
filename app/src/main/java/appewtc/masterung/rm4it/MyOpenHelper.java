1db2c249fefb35f6a31483c24402a827a08e90c6
masterUNG <phrombutr@gmail.com>
2016-03-25 15:16:48 +0700

    Create Table finsish

--- app/src/main/java/appewtc/masterung/rm4it/MyOpenHelper.java
+++ app/src/main/java/appewtc/masterung/rm4it/MyOpenHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by masterUNG on 2/17/16 AD.
  */
-public class MyOpenHelper extends SQLiteOpenHelper{
+public class MyOpenHelper extends SQLiteOpenHelper {
 
     //Explicit
     public static final String database_name = "rm4it.db";
@@ -23,6 +23,42 @@ public class MyOpenHelper extends SQLiteOpenHelper{
             "Work_Year text, " +
             "Email text);";
 
+    private static final String create_correct = "create table correctTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_environment = "create table environmentTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_governance = "create table governanceTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_internet = "create table internetTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_money = "create table moneyTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_network_intrusion = "create table network_intrusionTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_server_network = "create table server_networkTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_virus = "create table virusTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
+    private static final String create_wiless_network = "create table wiless_networkTABLE (" +
+            "_id integer primary key, " +
+            "Name text);";
+
 
     public MyOpenHelper(Context context) {
         super(context, database_name, null, database_version);
@@ -30,7 +66,16 @@ public class MyOpenHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-            sqLiteDatabase.execSQL(create_userTABLE);
+        sqLiteDatabase.execSQL(create_userTABLE);
+        sqLiteDatabase.execSQL(create_correct);
+        sqLiteDatabase.execSQL(create_environment);
+        sqLiteDatabase.execSQL(create_governance);
+        sqLiteDatabase.execSQL(create_internet);
+        sqLiteDatabase.execSQL(create_money);
+        sqLiteDatabase.execSQL(create_network_intrusion);
+        sqLiteDatabase.execSQL(create_server_network);
+        sqLiteDatabase.execSQL(create_virus);
+        sqLiteDatabase.execSQL(create_wiless_network);
     }
 
     @Override
