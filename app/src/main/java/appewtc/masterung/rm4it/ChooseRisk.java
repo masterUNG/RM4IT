64e021ca0972b4c10e9ca027955fc49daebada69
masterUNG <phrombutr@gmail.com>
2016-05-09 11:14:22 +0700

    Arrray

--- app/src/main/java/appewtc/masterung/rm4it/ChooseRisk.java
+++ app/src/main/java/appewtc/masterung/rm4it/ChooseRisk.java
@@ -2,19 +2,21 @@ package appewtc.masterung.rm4it;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class ChooseRisk extends AppCompatActivity {
+public class ChooseRisk extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
-    private String[] nameTableStrings;
+    private String[] nameTableStrings, userStrings;
     private Button risk1Button, risk2Button,
             risk3Button, risk4Button, risk5Button,
             risk6Button, risk7Button, risk8Button, risk9Button;
     private ImageView risk1ImageView, risk2ImageView, risk3ImageView,
             risk4ImageView, risk5ImageView, risk6ImageView, risk7ImageView,
             risk8ImageView, risk9ImageView;
+    private int indexAnInt = 0;
 
 
     @Override
@@ -27,7 +29,23 @@ public class ChooseRisk extends AppCompatActivity {
 
         setupNameTable();
 
+        userStrings = getIntent().getStringArrayExtra("User");
+
+        buttonController();
+
     }   // Main Method
+
+    private void buttonController() {
+        risk1Button.setOnClickListener(this);
+        risk2Button.setOnClickListener(this);
+        risk3Button.setOnClickListener(this);
+        risk4Button.setOnClickListener(this);
+        risk5Button.setOnClickListener(this);
+        risk6Button.setOnClickListener(this);
+        risk7Button.setOnClickListener(this);
+        risk8Button.setOnClickListener(this);
+        risk9Button.setOnClickListener(this);
+    }
 
     private void bindWidget() {
 
@@ -68,5 +86,53 @@ public class ChooseRisk extends AppCompatActivity {
         nameTableStrings[8] = "wiless_networkTABLE";
     }
 
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.button8:
+                indexAnInt = 0;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button9:
+                indexAnInt = 1;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button10:
+                indexAnInt = 2;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button11:
+                indexAnInt = 3;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button12:
+                indexAnInt = 4;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button13:
+                indexAnInt = 5;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button14:
+                indexAnInt = 6;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button15:
+                indexAnInt = 7;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+            case R.id.button16:
+                indexAnInt = 8;
+                risk1ImageView.setImageResource(R.drawable.mytrue);
+                break;
+
+
+        }   // switch
+
+
+    }   // onClick
 
 }   // Main Class
