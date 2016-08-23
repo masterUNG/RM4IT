90d12f4d256af7bb81b1469f8d0b3333f1d8b3db
masterUNG <phrombutr@gmail.com>
2016-08-23 10:39:31 +0700

    Add Lib GraphView1

--- app/src/main/java/appewtc/masterung/rm4it/Static.java
+++ app/src/main/java/appewtc/masterung/rm4it/Static.java
@@ -1,7 +1,11 @@
 package appewtc.masterung.rm4it;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Static extends AppCompatActivity {
 
@@ -9,6 +13,31 @@ public class Static extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_static);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 2),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6),
+                new DataPoint(5, 1),
+                new DataPoint(6, 5),
+                new DataPoint(7, 3),
+                new DataPoint(8, 3),
+                new DataPoint(9, 3)
+
+        });
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(9);
+        graph.setTitle("Master Title");
+
+
+        graph.addSeries(series);
+
+
     }   // Main Method
 
 }   // Main Class
