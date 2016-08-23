36ff769766deef7d31162c54d8caf30e61449753
masterUNG <phrombutr@gmail.com>
2016-08-23 09:49:19 +0700

    Add Lib GraphView

--- app/src/main/java/appewtc/masterung/rm4it/Static.java
+++ app/src/main/java/appewtc/masterung/rm4it/Static.java
@@ -9,5 +9,6 @@ public class Static extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_static);
-    }
-}
+    }   // Main Method
+
+}   // Main Class
